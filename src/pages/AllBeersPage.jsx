251ede062c34e4328ce2849d479918889b630606
YagoGu <yago.gutierrez15@gmail.com>
2023-08-25 17:29:52 +0200

    Iteration 4

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,45 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+
+useState
+
+function AllBeersPage() {
+    const apiURL = "https://ih-beers-api2.herokuapp.com/beers";
+
+    const[beers, setBeers] = useState([])
+
+    useEffect(() => {
+        fetch(apiURL)
+        .then((res) => {
+            return res.json ()
+        })
+        .then((data)=> {
+            return setBeers(data)
+        })
+
+        .catch((err) => {
+            console.log(err)
+        })
+
+    }, [apiURL])
+    
+    console.log(beers)
+
+    return (
+        <>
+        {
+            beers.map((beer) => {
+                return (
+                <div>
+                    <img src={beer.image_url} alt={beer.name} />
+                    <p>{beer.name}</p>
+                    <p>{beer.tagline}</p>
+                    <p>{beer.contributed_by}</p>
+                </div>
+                )
+            })
+        }
+        </>
+    )
+}
 
 export default AllBeersPage;
