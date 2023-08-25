a32db6858d35df47039195125199d88d7835432c
YagoGu <yago.gutierrez15@gmail.com>
2023-08-25 17:16:28 +0200

    Iteration 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,10 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import home from "./../assets/home-icon.png"
+
+function Navbar() {
+    return (
+        <Link to="/"><img src={home} alt="home" style={{backgroundColor: "blue"}}/></Link>
+    )
+}
 
 export default Navbar;
